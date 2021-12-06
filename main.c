647da2deaa2e73e859fb0960d14768cdca21f8b2
Yuqing Wu <ywu20@stuy.edu>
2021-12-06 15:00:02 -0500

    remove

--- main.c
+++ main.c
@@ -10,20 +10,25 @@ void CREATE(){
   int shmd = shmget(MEM, sizeof(int), IPC_CREAT | 0644);
   int * size = shmat(shmd,0,0);
   *size = 0;
+
   printf("Creating file . . . \n");
-  open("tele.txt", O_TRUNC | O_RDWR | O_CREAT, 0644);
+  int fd = open("tele.txt", O_TRUNC | O_RDWR | O_CREAT, 0644);
 
   printf("Creating semaphore . . .\n");
   int semd = semget(SEM,1,IPC_CREAT | 0644);
-  printf("sema pid : %d\n", semd);
-  union semun us;
-  us.val = 1;
-  int r = semctl(semd, 0, SETVAL, us);
-
+  close(fd);
 }
 
 void REMOVE(){
   printf("removes\n");
+  printf("Releasing shared memory . . . \n");
+  int shmd = shmget(MEM, 0,0);
+  shmctl(shmd,IPC_RMID,0);
+
+  printf("Removing semaphore . . .\n");
+  int semd = semget(SEM,1,0);
+  semctl(semd, IPC_RMID,0);
+
 }
 int main(int argc, char ** argv){
   // if arg is --create, creates the semaphore & shared memory
