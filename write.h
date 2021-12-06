b80e2c693a6f4798900304e147d44da2119c38fb
Yuqing Wu <ywu20@stuy.edu>
2021-12-06 15:15:08 -0500

    header

---
+++ write.h
@@ -0,0 +1,12 @@
+#ifndef WRITE_H
+#define WRITE_H
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/ipc.h>
+#include<sys/sem.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<string.h>
+#include<sys/shm.h>
+#include<errno.h>
+#endif
